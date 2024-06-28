6ca9bd7ad7f91abacd4d7f30dcd573756da6d2ef
yen936 <benjimagnelli@gmail.com>
2024-06-28 19:06:42 -0400

    recur search in rust

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,48 @@
 mod file_meta;
 use file_meta::FileMetaData; 
-// use std::fs::File;
-// use std::io::{BufRead, BufReader};
 use chrono::{DateTime, Utc};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use walkdir::WalkDir;
+
+// TODO:
+// Edgecases: permission issues, write out location (josn?)
 
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the efficient share_drive open search: ESOD!");
 
     let last_mod: DateTime<Utc> = Utc::now();
     let file = FileMetaData::new("example.txt".to_string(), 1024, last_mod);
     file.print_details();
 
-    //let source_dirs_file = File
+    let file = File::open("search_dirs.txt").expect("Failed to open file");
+    let reader = BufReader::new(file);
+
+    println!("Searching directories:");
+    for line in reader.lines() {
+        match line {
+            Ok(dir) => {
+                let dir = dir.trim();
+                println!("Directory: {}", dir);
+                search_files_recursively(dir);
+                println!();
+            }
+            Err(e) => eprintln!("Error reading line: {}", e),
+        }
+    }
+}
+
+// Define a function that takes a reference to a string slice as an argument
+fn search_files_recursively(dir: &str) {
+    // Start a for loop that iterates over the directory entries
+    for entry in WalkDir::new(dir)  // Create a new WalkDir instance for the given directory
+        .into_iter()  // Convert WalkDir into an iterator
+        .filter_map(|e| e.ok()) {  // Filter out errors and unwrap Ok values
+        
+        let path = entry.path();
+        if path.is_file() {
+            println!("  {}", path.display());
+        }
+    }
+    
 }

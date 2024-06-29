463681f95d64fc41348d2e89d787d8cd71b6a91e
yen936 <benjimagnelli@gmail.com>
2024-06-28 20:07:10 -0400

    timed search

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,46 @@
 mod file_meta;
-use file_meta::FileMetaData; 
-use chrono::{DateTime, Utc};
+//use file_meta::FileMetaData; 
+//use chrono::{DateTime, Utc};
+use std::time::{SystemTime};
 use std::fs::File;
+use std::path::Path;
 use std::io::{BufRead, BufReader};
 use walkdir::WalkDir;
+use std::io::{self, Write};
 
 // TODO:
 // Edgecases: permission issues, write out location (josn?)
 
 fn main() {
     println!("Welcome to the efficient share_drive open search: ESOD!");
-
-    let last_mod: DateTime<Utc> = Utc::now();
-    let file = FileMetaData::new("example.txt".to_string(), 1024, last_mod);
-    file.print_details();
+    // Search 
+    let tgt_file = get_user_input("Enter the file to search for: ");
+    let start = SystemTime::now();
 
     let file = File::open("search_dirs.txt").expect("Failed to open file");
     let reader = BufReader::new(file);
+    recurrsive_search(reader, &tgt_file);
 
-    println!("Searching directories:");
+    let end = SystemTime::now();
+    let duration = end.duration_since(start).unwrap();
+    println!("it took {} milliseconds", duration.as_millis());
+    
+}
+
+fn recurrsive_search(reader: BufReader<File>, target_file: &str) {
+    println!("Recursively Searching directories:");
     for line in reader.lines() {
         match line {
             Ok(dir) => {
                 let dir = dir.trim();
-                println!("Directory: {}", dir);
-                search_files_recursively(dir);
-                println!();
+                search_files_recursively(dir, &target_file);
             }
             Err(e) => eprintln!("Error reading line: {}", e),
         }
     }
 }
 
-// Define a function that takes a reference to a string slice as an argument
-fn search_files_recursively(dir: &str) {
+fn search_files_recursively(dir: &str, target_file: &str) {
     // Start a for loop that iterates over the directory entries
     for entry in WalkDir::new(dir)  // Create a new WalkDir instance for the given directory
         .into_iter()  // Convert WalkDir into an iterator
@@ -41,8 +48,22 @@ fn search_files_recursively(dir: &str) {
         
         let path = entry.path();
         if path.is_file() {
-            println!("  {}", path.display());
+            let path = Path::new(path);
+            let filename = path.file_name().unwrap();
+            if filename == target_file {
+                println!("Found {} at this path {}", filename.to_str().unwrap(), path.display());
+            }
+            //println!("  {}", path.display());
         }
     }
-    
+
+}
+
+fn get_user_input(prompt: &str) -> String {
+    print!("{}", prompt);
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    input.trim().to_string()
 }

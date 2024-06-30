edc5d02a81d1babb133407785c6337334c6abae3
yen936 <benjimagnelli@gmail.com>
2024-06-29 20:52:24 -0400

    conncurrency for dir traverse, dynamic thread number

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ use std::time::{SystemTime};
 use std::fs::File;
 use std::path::Path;
 use walkdir::WalkDir;
-use std::io::{self, Write, BufReader, BufRead};
+use std::io::{BufReader, BufRead};
 use std::sync::{Arc, Mutex};
-use std::collections::VecDeque;
-use std::path::PathBuf;
 //use rayon;
+use rayon::ThreadPoolBuilder; 
 use rayon::prelude::*;
 
 // TODO:
@@ -84,24 +83,33 @@ fn concurrent_search(reader: BufReader<File>, target_file: &str) {
     let target_file = Arc::new(target_file.to_string());
     let found_files = Arc::new(Mutex::new(Vec::new()));
 
-    dirs.par_iter().for_each(|dir| {
-        WalkDir::new(dir)
-            .into_iter()
-            .filter_map(|e| e.ok())
-            .par_bridge()
-            .for_each(|entry| {
-                let path = entry.path();
-                if path.is_file() {
-                    if let Some(filename) = path.file_name() {
-                        if filename == target_file.as_str() {
-                            let mut found_files = found_files.lock().unwrap();
-                            found_files.push(path.display().to_string());
+    // Customize ThreadPoolBuilder to set desired number of threads
+    let pool = ThreadPoolBuilder::new()
+        .num_threads(16) // Adjust the number of threads as needed
+        .build()
+        .unwrap();
+
+    pool.install(|| {
+        dirs.par_iter().for_each(|dir| {
+            WalkDir::new(dir)
+                .into_iter()
+                .filter_map(Result::ok)
+                .par_bridge()
+                .for_each(|entry| {
+                    let path = entry.path();
+                    if path.is_file() {
+                        if let Some(filename) = path.file_name() {
+                            if filename == target_file.as_str() {
+                                let mut found_files = found_files.lock().unwrap();
+                                found_files.push(path.display().to_string());
+                            }
                         }
                     }
-                }
-            });
+                });
+        });
     });
 
+   
     // Print out all found files (this could be written to a CSV or any other output)
     let found_files = found_files.lock().unwrap();
     for file in found_files.iter() {
